bddb11bb76f22163f4a7fdff55bd1b3f8a516265
Oskar Danovsky <oskar.danovsky@gmail.com>
2023-02-14 03:10:18 +0100

    template bot created

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
