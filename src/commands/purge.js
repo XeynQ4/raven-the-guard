ad9bfaf7aa47d381bdc8a1cd0ecd6fe7bb7a107d
Oskar Danovsky <oskar.danovsky@gmail.com>
2023-02-15 02:11:07 +0100

    added purge command and removed ping command

---
+++ src/commands/purge.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("purge")
+    .setDescription("Delete multiple messages")
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("Amount of messages to delete")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const amount = interaction.options.getInteger("amount");
+
+    if (amount <= 0)
+      return await interaction.reply({
+        content: "Amount must be a positive number",
+        ephemeral: true,
+      });
+
+    return await interaction.channel
+      .bulkDelete(amount)
+      .then((messages) =>
+        interaction.reply({
+          content: `Deleted ${messages.size} messages`,
+          ephemeral: true,
+        })
+      )
+      .catch(console.error);
+  },
+};
